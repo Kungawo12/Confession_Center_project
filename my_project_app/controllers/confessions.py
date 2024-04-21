1c663e1be667eb67597ced58c6f047033319ed10
Tenzin Kunga <officialtenzin.kunga@gmail.com>
2024-04-21 18:24:12 -0500

    like button probelem solved

--- my_project_app/controllers/confessions.py
+++ my_project_app/controllers/confessions.py
@@ -27,14 +27,20 @@ def create_shows():
     Confession.save_confession(data)
     return redirect('/dashboard')
 
-@app.route('/confession/<int:id>')
+@app.route('/confession/<int:id>', methods=['GET','POST'])
 def show_confession(id):
+    if request.method == 'POST':
+        user_id = request.form.get('user_id')  # Get user_id from form data
+        Confession.like_confession({'user_id': user_id, 'confession_id': id})
+        return redirect('/confession/'+str(id))  # Redirect back to the same page
+
     data = {
         'id': id
     }
     user_comment = Confession.get_all_comments({"confession_id": id})
     confession = Confession.show_one_confession(data)
-    return render_template('one_confession.html',confession=confession,comments=user_comment)
+    like_count = Confession.get_like_count(id)
+    return render_template('one_confession.html',confession=confession,comments=user_comment,like_count=like_count)
 
 @app.route('/confession/edit/<int:id>')
 def edit_confession(id):
@@ -77,4 +83,10 @@ def get_comments(id):
     Confession.save_comment(data)
     return redirect('/confession/'+str(id))
     
-        
+@app.route('/like_confession', methods=['POST'])
+def like_confession():
+    user_id = request.form.get('user_id')
+    confession_id = request.form.get('confession_id')
+    data = {'user_id': user_id, 'confession_id': confession_id}
+    Confession.like_confession(data)
+    return redirect('/confession/'+str(confession_id))
